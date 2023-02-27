a15ef647abf9252eceb13d43257a3ea600e267e5
Lyndon Orange <lyndonleeorange@gmail.com>
2023-02-26 21:47:43 -0800

    Updated sql and un commented for copy past to work

--- Students/Lyndon/DCI4-81.sql
+++ Students/Lyndon/DCI4-81.sql
@@ -4,23 +4,23 @@
 --Purpose: This is an example of how to create a table and add rows
 
 
---USE WideWorldImporters
---GO
+USE WideWorldImporters
+GO
 
---CREATE TABLE DEcodeIT(
+CREATE TABLE DEcodeIT(
 
---	[StudentID] [int] NOT NULL,
---	[StudentFirstName] [varchar] (50) Not Null,
---	[StudentLastName] [varchar] (50) Not Null,
---	[Studentaddress] [varchar] (70) Not Null,
---	[StudentCity] [varchar] (20) Null,
---	[StudentState] [varchar] (20) Null,
---	[StudentZip] [int] Not Null,
+	[StudentID] [int] NOT NULL,
+	[StudentFirstName] [varchar] (50) Not Null,
+	[StudentLastName] [varchar] (50) Not Null,
+	[Studentaddress] [varchar] (70) Not Null,
+	[StudentCity] [varchar] (20) Null,
+	[StudentState] [varchar] (20) Null,
+	[StudentZip] [int] Not Null,
 
---	CONSTRAINT [PK_DecodeIT] PRIMARY KEY CLUSTERED(
---	[StudentID] ASC
---	)
---)
+	CONSTRAINT [PK_DecodeIT] PRIMARY KEY CLUSTERED(
+	[StudentID] ASC
+	)
+)
 
 Insert Into [DEcodeIT] (StudentID, StudentFirstName, StudentLastName, Studentaddress, StudentCity, StudentZip) 
  Values(01, 'Lyndon', 'Orange', '4906 Aspen Drive', 'Omaha', 68157)
@@ -32,3 +32,5 @@ Insert Into [DEcodeIT] (StudentID, StudentFirstName, StudentLastName, Studentadd
  Values(04, 'John', 'Orange', '1235 Main Street', 'Clarksdale', 038145)
  Insert Into [DEcodeIT] (StudentID, StudentFirstName, StudentLastName, Studentaddress, StudentCity, StudentZip) 
  Values(05, 'Tony', 'Orange', '5431 Orange Ave', 'Oxford',038154)
+
+Select * FROM  DecodeIT
