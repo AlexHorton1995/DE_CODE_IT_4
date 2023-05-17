eeef4557f5901eb028d17f74e56b6b1100251573
Alex Horton <alex.d.horton95@gmail.com>
2023-05-17 10:21:28 -0500

    Added Globalization config to project.

--- FinalExam/applications/CodeBlackQuestionApi/CodeBlackQuestionApi/Models/CustomerModel.cs
+++ FinalExam/applications/CodeBlackQuestionApi/CodeBlackQuestionApi/Models/CustomerModel.cs
@@ -7,7 +7,7 @@
         */
 
         public int CustomerId { get; set; }
-        public stri5ng? FirstName { get; set; }
+        public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
