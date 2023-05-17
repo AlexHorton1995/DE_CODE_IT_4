7cd79160428fb0eca6e633e584d1d0c15a729496
Alex Horton <alex.d.horton95@gmail.com>
2023-05-17 18:20:16 -0500

    Publishing changes without testing

--- FinalExam/applications/CodeBlackQuestionApi/CodeBlackQuestionApi/Models/CustomerModel.cs
+++ FinalExam/applications/CodeBlackQuestionApi/CodeBlackQuestionApi/Models/CustomerModel.cs
@@ -6,7 +6,7 @@
             Anjali's table
         */
 
-        public int CustomerId { get; set; }
+        public string? CustomerId { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public string? Address { get; set; }
